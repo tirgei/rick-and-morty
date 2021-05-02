edcbc3d9b837c8a83fbb5f3295424ffcc5628f3c
Vincent Tirgei <tirgeic@gmail.com>
2021-05-02 16:54:28 +0300

    Setup BaseFragment

---
+++ app/src/main/java/com/tirgei/rickandmorty/ui/base/BaseFragment.kt
@@ -0,0 +1,32 @@
+package com.tirgei.rickandmorty.ui.base
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.viewbinding.ViewBinding
+
+@Suppress("UNCHECKED_CAST")
+abstract class BaseFragment<VB : ViewBinding> : Fragment() {
+    private var _binding: ViewBinding? = null
+    abstract val bindingInflater: (LayoutInflater, ViewGroup?, Boolean) -> VB
+
+    protected val binding: VB
+        get() = _binding as VB
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = bindingInflater.invoke(inflater, container, false)
+        return requireNotNull(_binding).root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+}
